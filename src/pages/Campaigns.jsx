7f55b3df8bd019b5961e6ca3137adece272e3473
Nawel Benaicha <nawelbenaicha@MacBook-Pro-de-Nawel.local>
2022-07-23 21:20:39 +0200

    STYLE FOR EVERY PAGES

--- src/pages/Campaigns.jsx
+++ src/pages/Campaigns.jsx
@@ -5,7 +5,7 @@ const Campaigns = () => {
   return (
     <div className="bg-creme h-screen">
       <NavBar />
-      <div className="flex flex-col mt-20 ">
+      <div className="flex flex-col mt-10 ">
         <span className="bg-dark h-2 w-screen mobS:w-screen"></span>
         <Gallery />
       </div>
--- src/pages/Polas.jsx
+++ src/pages/Polas.jsx
@@ -4,9 +4,9 @@ import axios from "axios";
 
 const Polas = () => {
   const [allimages, setAllImages] = useState([]);
+  const [allmesures, setAllMesures] = useState([]);
 
   // axios pour récupèrer les images par page
-
   useEffect(() => {
     const getallImages = async () => {
       // verifier que le .env soit bien identique au lien postman qui fonctionne
@@ -18,30 +18,55 @@ const Polas = () => {
     getallImages();
   }, []);
 
+  // axios pour récupèrer les mesures
+  useEffect(() => {
+    const getallMesures = async () => {
+      // verifier que le .env soit bien identique au lien postman qui fonctionne
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_API_URL}api/mesures`
+      );
+      setAllMesures(data);
+    };
+    getallMesures();
+  }, []);
+
   return (
     <div className="bg-creme w-screen h-screen">
       <NavBar />
       <div className="flex pb-10 mt-6 mobS:bg-creme ">
-        <div className="grid grid-cols-2 grid-rows-2  m-auto shadow-lg gap-10  mt-10 h-5/6 place-items-center mobS:grid-cols-1 ">
+        <div className="grid grid-cols-2 grid-rows-2 m-auto shadow-xl mt-5 rounded-lg h-5/6 place-items-center mobS:grid-cols-1 ">
           {allimages &&
             allimages.map(({ name, id }) => (
-              <div key={id} className="group cursor-pointer ">
-                <img
-                  className="object-cover rounded-lg bg-white h-72 hover:opacity-30 "
-                  src={name}
-                />
-                <span className="absolute -mt-44 hover:text-transparent translate-x-2">
-                  <a className="font-body flex z-50 flex-col font-extrabold text-xl opacity-0 group-hover:opacity-100 mobS:hidden">
-                    Height: 166cm - 5.6 UK
-                  </a>
-                  <a className="font-body font-extrabold text-xl opacity-0 group-hover:opacity-100 mobS:hidden ">
-                    Shoes: 38 EU - 5 UK - 7 US
-                  </a>
-                </span>
+              <div key={id}>
+                <a href="https://www.instagram.com/nahabee/" target="_blank">
+                  {" "}
+                  <img
+                    className="object-cover cursor-pointer rounded-lg bg-white m-6 h-64 hover:scale-110 transition hover:duration-700 hover:ease-in-out"
+                    src={name}
+                  />
+                </a>
               </div>
             ))}
         </div>
-        <h1> </h1>
+        <div className="w-1/2 flex flex-col">
+          <h1 className=" font-title text-7xl w-1/2 mt-8 opacity-80">
+            MODEL stats,
+          </h1>
+          <div className="flex flex-col group mt-5">
+            <div className=" h-60 w-32 flex bg-stone-900 opacity-90 " />
+            {allmesures &&
+              allmesures.map(({ name, description, id }) => (
+                <div key={id} className="flex flex-col mt-[-3.7rem]  ">
+                  <a className="font-body z-50 text-xl mobS:hidden ml-40 tracking-widest">
+                    <span className="font-subtitle opacity-80 tracking-normal ">
+                      {name}
+                    </span>
+                    : {description}
+                  </a>
+                </div>
+              ))}
+          </div>
+        </div>
       </div>
     </div>
   );
