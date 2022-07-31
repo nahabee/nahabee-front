f4ff1845c281d0984d62835f23e68f43050d1835
Nawel Benaicha <nawelbenaicha@MacBook-Pro-de-Nawel.local>
2022-08-01 00:50:38 +0200

    cleaning

--- src/pages/Shoot.jsx
+++ src/pages/Shoot.jsx
@@ -7,13 +7,13 @@ import CurrentDataContext from "../context/currentData";
 const Shoot = () => {
   const { shoot } = useContext(CurrentDataContext);
   return (
-    <div className="h-screen w-screen bg-creme ">
+    <div className="h-screen w-screen bg-creme mobS:rounded-lg ">
       <NavBar />
       <div className="flex justify-center shadow-2xl rounded-lg pt-20 mx-20 mt-5 bg-creme mobS:hidden ">
         <Carousel />
       </div>
       {/* mobile version */}
-      <div className="xs:hidden bg-grey mobS:pt-10 mobS:rounded-lg">
+      <div className="xs:hidden bg-grey mobS:pt-14 mobS:rounded-lg">
         {shoot && shoot.map(({ name, id }) => <img key={id} src={name} />)}
       </div>
     </div>
